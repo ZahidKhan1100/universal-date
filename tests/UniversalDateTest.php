9cccd93c460cfa3648e4892b72c68f23417e8e70
ZahidKhan1100 <kzahid416@gmail.com>
2025-01-05 22:15:24 +0100

    Update test cases with comprehensive time calculations

--- tests/UniversalDateTest.php
+++ tests/UniversalDateTest.php
@@ -55,44 +55,62 @@ class UniversalDateTest extends TestCase
         $this->assertEquals('2 hours ago', $date->toTimeAgo());
     }
 
-    public function it_handles_time_ago_for_future_dates()
+    /**
+    * @test
+    * @dataProvider timeDifferenceProvider
+    */
+    public function it_handles_time_ago_for_future_dates($input, $expected)
     {
-        // Test very near future
-        $this->assertEquals('soon', UniversalDate::make('+10 seconds')->toTimeAgo());
-        $this->assertEquals('soon', UniversalDate::make('+45 seconds')->toTimeAgo());
-        
-        // Test minutes
-        $this->assertEquals('in 1 minute', UniversalDate::make('+1 minute')->toTimeAgo());
-        $this->assertEquals('in 30 minutes', UniversalDate::make('+30 minutes')->toTimeAgo());
-        
-        // Test hours and their boundaries
-        $this->assertEquals('in 1 hour', UniversalDate::make('+45 minutes')->toTimeAgo());
-        $this->assertEquals('in 1 hour', UniversalDate::make('+59 minutes')->toTimeAgo());
-        $this->assertEquals('in 1 hour', UniversalDate::make('+1 hour')->toTimeAgo());
-        $this->assertEquals('in 1 hour', UniversalDate::make('+1 hour 15 minutes')->toTimeAgo());
-        $this->assertEquals('in 2 hours', UniversalDate::make('+1 hour 45 minutes')->toTimeAgo());
-        $this->assertEquals('in 2 hours', UniversalDate::make('+2 hours')->toTimeAgo());
-        $this->assertEquals('in 23 hours', UniversalDate::make('+23 hours')->toTimeAgo());
-        
-        // Test days and their boundaries
-        $this->assertEquals('in 1 day', UniversalDate::make('+24 hours')->toTimeAgo());
-        $this->assertEquals('in 1 day', UniversalDate::make('+25 hours')->toTimeAgo());
-        $this->assertEquals('in 1 day', UniversalDate::make('+1 day')->toTimeAgo());
-        $this->assertEquals('in 2 days', UniversalDate::make('+2 days')->toTimeAgo());
-        $this->assertEquals('in 6 days', UniversalDate::make('+6 days')->toTimeAgo());
-        
-        // Test months and their boundaries
-        $this->assertEquals('in 1 month', UniversalDate::make('+7 days')->toTimeAgo());
-        $this->assertEquals('in 1 month', UniversalDate::make('+30 days')->toTimeAgo());
-        $this->assertEquals('in 1 month', UniversalDate::make('+1 month')->toTimeAgo());
-        $this->assertEquals('in 2 months', UniversalDate::make('+2 months')->toTimeAgo());
-        $this->assertEquals('in 11 months', UniversalDate::make('+11 months')->toTimeAgo());
-        
-        // Test years and their boundaries
-        $this->assertEquals('in 1 year', UniversalDate::make('+12 months')->toTimeAgo());
-        $this->assertEquals('in 1 year', UniversalDate::make('+1 year')->toTimeAgo());
-        $this->assertEquals('in 2 years', UniversalDate::make('+2 years')->toTimeAgo());
-        $this->assertEquals('in 5 years', UniversalDate::make('+5 years')->toTimeAgo());
+        $result = UniversalDate::make($input)->toTimeAgo();
+        $this->assertEquals($expected, $result, "Failed asserting that '{$result}' matches expected '{$expected}' for input '{$input}'");
+    }
+
+    public function timeDifferenceProvider()
+    {
+        return [
+            // Very near future
+            'seconds_10' => ['+10 seconds', 'soon'],
+            'seconds_45' => ['+45 seconds', 'soon'],
+            
+            // Minutes
+            'minute_1' => ['+1 minute', 'in 1 minute'],
+            'minute_30' => ['+30 minutes', 'in 30 minutes'],
+            'minute_44' => ['+44 minutes', 'in 44 minutes'],
+            
+            // Hour boundaries
+            'hour_0.75' => ['+45 minutes', 'in 1 hour'],
+            'hour_1' => ['+1 hour', 'in 1 hour'],
+            'hour_1.25' => ['+1 hour 15 minutes', 'in 1 hour'],
+            'hour_1.75' => ['+1 hour 45 minutes', 'in 2 hours'],
+            'hour_2' => ['+2 hours', 'in 2 hours'],
+            'hour_23' => ['+23 hours', 'in 23 hours'],
+            'hour_23.5' => ['+23 hours 30 minutes', 'in 1 day'],
+            
+            // Day boundaries
+            'day_1_exact' => ['+24 hours', 'in 1 day'],
+            'day_1_over' => ['+25 hours', 'in 1 day'],
+            'day_1_native' => ['+1 day', 'in 1 day'],
+            'day_2' => ['+2 days', 'in 2 days'],
+            'day_6' => ['+6 days', 'in 6 days'],
+            'day_29' => ['+29 days', 'in 29 days'],
+            
+            // Month boundaries
+            'month_1_exact' => ['+30 days', 'in 1 month'],
+            'month_1_over' => ['+32 days', 'in 1 month'],
+            'month_1_native' => ['+1 month', 'in 1 month'],
+            'month_2' => ['+2 months', 'in 2 months'],
+            'month_11' => ['+11 months', 'in 11 months'],
+            'month_11.5' => ['+11 months 15 days', 'in 1 year'],
+            
+            // Year boundaries
+            'year_1_exact' => ['+12 months', 'in 1 year'],
+            'year_1_native' => ['+1 year', 'in 1 year'],
+            'year_1_over' => ['+13 months', 'in 1 year'],
+            'year_2' => ['+2 years', 'in 2 years'],
+            'year_2.5' => ['+2 years 6 months', 'in 2 years'],
+            'year_5' => ['+5 years', 'in 5 years']
+        ];
+    }
     }
 
     /** @test */
